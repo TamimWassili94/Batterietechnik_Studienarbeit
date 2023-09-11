4c10ac8dce0988cfa132c46ac5c69670a70b6981
Taras1994m <92815911+Taras1994m@users.noreply.github.com>
2023-09-11 19:24:39 +0200

    U_R1 mit euler und trapezoid integration implementiert

--- Backend/Ohmsche Groeßen.py
+++ Backend/Ohmsche Groeßen.py
@@ -1,61 +1,92 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from transfer_functions import RK4
+from transfer_functions import perform_integration_rk4
 
 Battery_Dataframe = pd.read_csv('BatteryData_2.csv')
 
-def U_R1(dataframe):
-    integrated_value = 0  # Initialize the integrated value
-    dt = 0.5  # Time step for integration
+def simulate_system_with_euler(Battery_Dataframe):
+    initial_output = 0  # initial value for output
+    dt = 0.5  # time step
+    output_list = []
 
-    # Define the function to be integrated with RK4
-    f = lambda y, u: u
+    for index, row in Battery_Dataframe.iterrows():
+        R1 = row['R1']
+        I_Zelle = row['Strom [A]']
+        C1 = row['C1']
 
-    # Initialize arrays for holding input and output values
-    input_signal = dataframe['irgendwas2'].values  # Assuming 'irgendwas2' is what we want to integrate
-    output_signal = perform_integration_rk4(dataframe, 'irgendwas2', 'integrated_irgendwas2', dt)
+        # Step 1: Divide output by R1
+        Value1 = initial_output / R1 if R1 != 0 else 0
 
-    # Loop through the DataFrame rows
-    for index, row in dataframe.iterrows():
-        # Update 'irgendwas' and 'irgendwas2'
-        dataframe.at[index, 'I_R1'] = dataframe.at[index, 'U_R1'] / row['R1']
-        dataframe.at[index, 'irgendwas'] = row['Strom [A]'] - dataframe.at[index, 'I_R1']
-        dataframe.at[index, 'irgendwas2'] = dataframe.at[index, 'irgendwas'] / row['C1']
+        # Step 2: Subtract Value1 from I_Zelle
+        Value2 = I_Zelle - Value1
 
-        # Integrate 'irgendwas2' using RK4
-        integrated_value = output_signal[index]  # Retrieve the integrated value for the current index
+        # Step 3: Divide Value2 by C1
+        Value3 = Value2 / C1 if C1 != 0 else 0
 
-        # Update U_R1, I_R1, and integrated_value ('dU_R1/dt')
-        dataframe.at[index, 'U_R1'] = row['R1'] * integrated_value
-        dataframe.at[index, 'I_R1'] = dataframe.at[index, 'U_R1'] / row['R1']
-        dataframe.at[index, 'dU_R1/dt'] = integrated_value
+        # Step 4: Euler integration to produce new output
+        new_output = initial_output + Value3 * dt
 
-    print(dataframe)  # Print the DataFrame for debugging
-    return dataframe  # Return the modified DataFrame
+        # Save new output for the next iteration
+        initial_output = new_output
+        output_list.append(initial_output)
 
-def perform_integration_rk4(dataframe, column_to_manipulate, new_columnname, dt=0.5):
-    integrated_dataframe = dataframe.copy()
-    input_signal = integrated_dataframe[column_to_manipulate].values
+    # Add the output signal to the original dataframe
+    Battery_Dataframe['output'] = output_list
 
-    f = lambda y, u: u  # Here, 'u' is the input from 'input_signal'
-
-    output_signal = RK4(f, 0, dt, len(input_signal), input_signal)  # Start from 0
-
-    integrated_dataframe[new_columnname] = output_signal
-    return integrated_dataframe[new_columnname]
+    return Battery_Dataframe
 
 
 
-batterydata = U_R1(Battery_Dataframe)
+Battery_Dataframe = simulate_system_with_euler(Battery_Dataframe)
 
-plt.plot(batterydata.index, batterydata["U_R1"])
-
-# Add labels and title
-plt.xlabel("Index")
-plt.ylabel("U_R1")
-plt.title("Plot of U_R1")
-
-# Show the plot
+# Assuming your DataFrame is called Battery_Dataframe
+ax = Battery_Dataframe.plot(x='Zeit [s]', y='output')
+plt.xlabel('Zeit [S]')
+plt.ylabel('Output')
+plt.title('Output over Time')
+ax.grid(True)  # This line adds grid lines
 plt.show()
 
 
+def simulate_system_with_trapezoidal(Battery_Dataframe):
+    initial_output = 0  # initial value for output
+    dt = 0.5  # time step
+    prev_value3 = 0  # store the previous value for the Trapezoidal rule
+    output_list = []
+
+    for index, row in Battery_Dataframe.iterrows():
+        R1 = row['R1']
+        I_Zelle = row['Strom [A]']
+        C1 = row['C1']
+
+        # Step 1: Divide output by R1
+        Value1 = initial_output / R1 if R1 != 0 else 0
+
+        # Step 2: Subtract Value1 from I_Zelle
+        Value2 = I_Zelle - Value1
+
+        # Step 3: Divide Value2 by C1
+        Value3 = Value2 / C1 if C1 != 0 else 0
+
+        # Step 4: Trapezoidal Rule integration to produce new output
+        new_output = initial_output + (Value3 + prev_value3) * dt / 2
+
+        # Save new output for the next iteration
+        initial_output = new_output
+        prev_value3 = Value3
+        output_list.append(initial_output)
+
+    # Add the output signal to the original dataframe
+    Battery_Dataframe['output'] = output_list
+
+    return Battery_Dataframe
+
+Battery_Dataframe = simulate_system_with_trapezoidal(Battery_Dataframe)
+
+# Assuming your DataFrame is called Battery_Dataframe
+ax = Battery_Dataframe.plot(x='Zeit [s]', y='output')
+plt.xlabel('Zeit [S]')
+plt.ylabel('Output')
+plt.title('Output over Time')
+ax.grid(True)  # This line adds grid lines
+plt.show()
